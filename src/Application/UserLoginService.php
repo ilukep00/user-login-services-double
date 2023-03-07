b2c7b6cb61c0692f7dbe06922b4fb3bf12b0057a
iñaki <lukepastor00@gmail.com>
2023-03-07 12:15:37 +0000

    TDD manualLogin S2 Test Passed for users already loggers

--- src/Application/UserLoginService.php
+++ src/Application/UserLoginService.php
@@ -1,14 +1,27 @@
 <?php
 
 namespace UserLoginService\Application;
+use Exception;
+use UserLoginService\Domain\User;
 
 class UserLoginService
 {
     private array $loggedUsers = [];
 
-    public function manualLogin(User $user): string
+    public function manualLogin(User $user)
     {
-        return "user logged";
+        throw new Exception("User already logged in");
     }
+
+    /**
+     * @return array
+     */
+    public function getLoggedUsers(): array
+    {
+        return $this->loggedUsers;
+    }
+
+
+
 
 }
--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace UserLoginService\Tests\Application;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function userIsLoggedIn()
+    public function errorWhileLoginUserIfAlreadyLoggedIn()
     {
         $userLoginService = new UserLoginService();
+        $user = new User("username");
 
-        $this->expectExceptionMessage('User already logged in');
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("User already logged in");
 
-        $userLoginService->manualLogin("lukepastor");
+        $userLoginService->manualLogin($user);
     }
 
 
